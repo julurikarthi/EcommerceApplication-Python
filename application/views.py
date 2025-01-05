4196859d53e7d06b6d9e85be4f5fb9f710dc575a
karthik juluri <karthik.juluri@publix.com>
2025-01-04 19:46:50 -0500

    testing

--- application/views.py
+++ application/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 
 def data_view(request):
     return JsonResponse({
-        "message": "Hello, World!",
+        "message": "Hello, World!e",
         "status": "success",
         "data": {"id": 1, "name": "Sample Item"}
     })
