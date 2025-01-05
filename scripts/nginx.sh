9858a8ec13349b13cf45d82800d008e90ade8923
karthik juluri <karthik.juluri@publix.com>
2025-01-04 22:26:26 -0500

    testing

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,9 +4,9 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/EcommerceApplication-Python/nginx/nginx.conf /etc/nginx/sites-available/blog
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+sudo cp /home/ubuntu/EcommerceApplication-Python/nginx/nginx.conf /etc/nginx/sites-available/EcommerceApplication
+sudo ln -s /etc/nginx/sites-available/EcommerceApplication /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/EcommerceApplication /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
