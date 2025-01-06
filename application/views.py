24aca41be565481d585c9301c74f7ab1228180bd
karthik juluri <karthik.juluri@publix.com>
2025-01-06 10:25:32 -0500

    workig

--- application/views.py
+++ application/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse, HttpResponseForbidden
 
 def data_view(request):
     return JsonResponse({
-        "message": "Welcome to the world",
+        "message": "start the project",
         "status": "success",
         "data": {"id": 1, "name": "Sample Item"}
     })
