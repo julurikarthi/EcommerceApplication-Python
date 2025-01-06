5dd09d70799126f7badeb06386745f0410b9a1ab
karthik juluri <karthik.juluri@publix.com>
2025-01-05 22:43:49 -0500

    workig

--- application/urls.py
+++ application/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('data/', views.data_view, name='data_view'),
-    path('view_logs/', views.view_logs, name='view_logs'),
 ]
--- application/views.py
+++ application/views.py
@@ -8,22 +8,3 @@ def data_view(request):
         "status": "success",
         "data": {"id": 1, "name": "Sample Item"}
     })
-
-def view_logs(request):
-
-    # Define log file paths
-    log_files = {
-        "nginx_error": "/var/log/nginx/error.log",
-        "gunicorn_error": "/var/log/gunicorn-error.log",
-        "django_debug": "/var/log/django/debug.log",
-    }
-
-    logs_content = {}
-    for name, path in log_files.items():
-        if os.path.exists(path):
-            with open(path, "r") as log_file:
-                logs_content[name] = log_file.readlines()[-50:]  # Fetch the last 50 lines
-        else:
-            logs_content[name] = f"{path} does not exist."
-
-    return JsonResponse(logs_content)
