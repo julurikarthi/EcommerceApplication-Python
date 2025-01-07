e45efaca0c8d6f56fc9f5778f7f0493a3c966d35
karthik juluri <karthik.juluri@publix.com>
2025-01-07 14:14:56 -0500

    workig

--- application/views.py
+++ application/views.py
@@ -1,10 +1,5 @@
 from django.http import JsonResponse, HttpResponseForbidden
 
 def data_view(request):
-    # Intentionally raise an error to simulate a crash
     raise ValueError("Simulated crash for log testing")
-    return JsonResponse({
-        "message": "start the project",
-        "status": "success",
-        "data": {"id": 1, "name": "Sample Item"}
-    })
+    
