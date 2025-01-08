b1fa0ab61db310cb798209ae22c97c6fc3dc31c9
karthik juluri <karthik.juluri@publix.com>
2025-01-08 08:34:30 -0500

    updating yml

--- application/views.py
+++ application/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse, HttpResponseForbidden
 def data_view(request):
     # Intentionally raise an error to simulate a crash
     return JsonResponse({
-        "message": "start the project six",
+        "message": "start the project seven",
         "status": "success",
         "data": {"id": 1, "name": "Sample Item"}
     })
