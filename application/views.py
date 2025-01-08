6dabb8987f3382e7ebe616f1463fe20890f09622
karthik juluri <karthik.juluri@publix.com>
2025-01-08 17:40:37 -0500

    mounting

--- application/views.py
+++ application/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse, HttpResponseForbidden
 def data_view(request):
     # Intentionally raise an error to simulate a crash
     return JsonResponse({
-        "message": "start the project eleven",
+        "message": "start the project newele",
         "status": "success",
         "data": {"id": 1, "name": "Sample Item"}
     })
