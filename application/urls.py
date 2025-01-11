c50d06144cc015bb46a8e1a8d50c69ea0b34a487
karthik juluri <karthik.juluri@publix.com>
2025-01-10 20:43:53 -0500

    working on db intergration

--- application/urls.py
+++ application/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('data/', views.data_view, name='data_view'),
-    path('createDatabase/', views.createDatabase, name='create_database_view'),
+    path('createDatabase/', views.createDatabase, name='create_database'),
 ]
