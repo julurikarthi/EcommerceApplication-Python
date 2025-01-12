378bac701a533d525442a8fcef74ae0e4d48fc6a
karthik juluri <karthik.juluri@publix.com>
2025-01-12 11:40:57 -0500

    integrating s3 bucket

--- application/views.py
+++ application/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 from pymongo.errors import ConnectionFailure
 import json
 from django.conf import settings
-import boto3
 from django.core.files.storage import default_storage
 
 MONGODB_CONNECTION_STRING = "mongodb://18.188.42.21:27017/"
