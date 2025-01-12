9dd397a4bb1747d6caf048c924f94ae44d0e41c5
karthik juluri <karthik.juluri@publix.com>
2025-01-12 10:17:38 -0500

    integrating s3 bucket

--- application/urls.py
+++ application/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 import os
 
 urlpatterns = [
@@ -9,6 +7,3 @@ urlpatterns = [
     path('createStore/', views.createStore, name='createStore'),
     path('uploadImage/', views.uploadImage, name='uploadImage'),
 ]
-
-if settings.DEBUG:  # This check ensures it only happens in development
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
