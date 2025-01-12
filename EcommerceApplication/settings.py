37854f332ccdd847d9c06c6d9744673ce3015024
karthik juluri <karthik.juluri@publix.com>
2025-01-12 11:54:11 -0500

    integrating s3 bucket

--- EcommerceApplication/settings.py
+++ EcommerceApplication/settings.py
@@ -51,8 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'application',
     'storages',
+    'application',
 ]
 
 MIDDLEWARE = [
