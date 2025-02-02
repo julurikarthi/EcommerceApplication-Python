cd2857feec0b94302eba5b742eb281423062069f
karthik juluri <karthik.juluri@publix.com>
2025-02-02 09:51:08 -0500

    working on hosting

--- EcommerceApplication/settings.py
+++ EcommerceApplication/settings.py
@@ -96,7 +96,7 @@ CORS_ALLOWED_ORIGINS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'application/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'application/templates/react')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
