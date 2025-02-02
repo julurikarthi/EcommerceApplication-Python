889d1996434bff2fdd4e80026427c951ce55ddcf
karthik juluri <karthik.juluri@publix.com>
2025-02-02 09:13:00 -0500

    working on hosting

--- EcommerceApplication/settings.py
+++ EcommerceApplication/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'EcommerceApplication.urls'
